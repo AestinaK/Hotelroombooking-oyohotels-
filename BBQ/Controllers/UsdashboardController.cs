423c7fd22df9c4e45b15aa2e363671d8ee9766bc
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-05-18 09:40:18 +0545

    Add project files.

---
+++ BBQ/Controllers/UsdashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBQ.Controllers
+{
+    public class UsdashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BBQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BBQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
