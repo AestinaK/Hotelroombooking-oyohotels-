360720ab504531689ea546dbaa7a1581552f455d
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-05-18 13:06:11 +0545

    landing page and other content

--- BBQ/Program.cs
+++ BBQ/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Usdashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Landing}/{action=Index}/{id?}");
 
 app.Run();
