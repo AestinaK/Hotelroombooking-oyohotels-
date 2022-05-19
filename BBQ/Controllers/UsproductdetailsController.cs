e359ae541c3a36d81c26734c81fa73482dd557f0
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-05-19 13:03:31 +0545

    completed upto product details

---
+++ BBQ/Controllers/UsproductdetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBQ.Controllers
+{
+    public class UsproductdetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BBQ/Views/Usproductdetails/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBQ.Views.Usproductdetails
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
