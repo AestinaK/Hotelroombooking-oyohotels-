085b9da898f473809567c746af9bb6f2f7c60174
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-05-29 19:18:29 +0545

    database setup completed

--- BBQ/Controllers/UsuserloginController.cs
+++ BBQ/Controllers/UsuserloginController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBQ.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BBQ.Controllers
 {
     public class UsuserloginController : Controller
     {
+        DataContext dal = new DataContext();
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult insert(Ususerlogin vmodel)
+        {
+            try
+            {
+
+                dal.Ususerlogins.Add(vmodel);
+                dal.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return Redirect("/Landing/Index");
+
+        }
     }
+
 }
---
+++ BBQ/Models/Ususerlogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BBQ.Models
+{
+    [Table("logintable")]
+    public class Ususerlogin
+    {
+        [Key]
+        public int id{ get; set;}
+        public string ename{ get; set;}
+
+        public string address { get; set; }
+    }
+}
