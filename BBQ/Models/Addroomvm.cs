6aa71e2f34b6072441417431b46f60f215b2584a
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-06-26 22:28:23 +0545

    model added for room

---
+++ BBQ/Models/Addroomvm.cs
@@ -0,0 +1,10 @@
+﻿namespace BBQ.Models
+{
+    public class Addroomvm
+    {
+        public int roomno { get; set; }
+        public string  typename { get; set; }
+        public string hname { get; set; }
+
+    }
+}
