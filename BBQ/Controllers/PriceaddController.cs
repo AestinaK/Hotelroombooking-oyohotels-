dc566d5409c1061d1859f7c59a8bed02f8c0051d
Bishwadip <karkibishwadip960@gmail.com>
2022-06-27 15:19:41 +0545

    price for room completed

--- BBQ/Controllers/PriceaddController.cs
+++ BBQ/Controllers/PriceaddController.cs
@@ -13,5 +13,48 @@ namespace BBQ.Controllers
             ViewBag.types = listtype;
             return View();
         }
+
+        public IActionResult Addprice( Pricevm vm)
+        {
+            var hotel = dal.Hotelss.Where(x => x.name == vm.hname).ToList();
+            var hid = hotel[0].hid;
+            var roomtyp=dal.Roomtypes.Where(x => x.type == vm.roomtype).ToList();
+            var rtid = roomtyp[0].rtid;
+
+
+            try
+            {
+                Price login = new Price()
+                {
+                    hid=hid,
+                    rtid=rtid,
+                    price=vm.price
+
+                };
+
+                if (vm.roomtype.Equals("Classic"))
+                {
+                    var hot = from x in dal.Hotelss where x.hid == hid select x;
+
+                    foreach(var item in hot)
+                    {
+                        item.price = vm.price;
+
+                    }
+                    dal.SaveChanges();
+                }
+
+                dal.Prices.Add(login);
+                dal.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+  
+            return View();
+        }
     }
 }
