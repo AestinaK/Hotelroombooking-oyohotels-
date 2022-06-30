7ebe6c955aeeb3e1a41defcfc9b00e5e22dc7042
ankit shiwakoti <shiwakotiankit@gmail.com>
2022-06-30 07:00:55 +0545

    hehe

--- BBQ/Controllers/PhotoController.cs
+++ BBQ/Controllers/PhotoController.cs
@@ -1,55 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBQ.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 
 
 
 namespace BBQ.Controllers
 {
-    private readonly IWebHostEnvironment _webHostEnvironment;
-    public ReportController(IWebHostEnvironment webHostEnvironment)
-    {
-        _webHostEnvironment = webHostEnvironment;
-    }
+    /* private readonly IWebHostEnvironment _webHostEnvironment;
+     public ReportController(IWebHostEnvironment webHostEnvironment)
+     {
+         _webHostEnvironment = webHostEnvironment;
+     }*/
 
 
     public class PhotoController : Controller
     {
-       
-           [HttpPost]
-            [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Accident(AccidentViewModel AccidentReport)
+        DataContext dal = new DataContext();
+        public IActionResult Index()
         {
-            string folderpath = "image/";
-            if (ModelState.IsValid)
-            {
-
-                Accident accident = new Accident()
-                {
-                    Number = AccidentReport.Number,
-                    Image_Urls = await UploadImage(folderpath, AccidentReport.file),
-                    Contact = AccidentReport.Contact,
-                    Location = AccidentReport.Location,
-                    ReportersName = AccidentReport.ReportersName,
-                };
-                //product.ImageUrl = "hello";//await UploadImage(folderpath, file); //use the function here from Doctor Patient Model
-
-                dal.accidents.Add(accident);
-                dal.SaveChanges();
-
-                return RedirectToAction("Index", "Dash1");
-            }
             return View();
-            //return View(AccidentReport);
         }
-
-
-
-        public async Task<string> UploadImage(string folderpath, IFormFile file)
-        {
-            folderpath += Guid.NewGuid().ToString() + "_" + file.FileName;
-            string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folderpath);
-            await file.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
-            return "/" + folderpath;
-        }
+       
     }
+
 }
